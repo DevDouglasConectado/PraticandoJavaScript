ae5c7151526ed5d47b8603b6d4968b97764b6697
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-01-08 15:24:42 -0300

    funcao

---
+++ funcaoParametrizadas/funcaoParametrizadas.js
@@ -0,0 +1,25 @@
+
+/*const VALOR_PADRAO=0
+
+function soma(n1=VALOR_PADRAO, n2=VALOR_PADRAO){
+    let res
+    res=n1+n2
+    return res
+}
+
+let resultado_soma=soma(5,10)
+console.log(resultado_soma)*/
+
+const VALOR_PADRAO=0
+
+function add(v){
+    valor+=v
+}
+let valor=0
+console.log(valor)
+
+add(15)
+console.log(valor)
+
+add(15)
+console.log(valor)
