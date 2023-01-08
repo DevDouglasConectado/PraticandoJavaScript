ce8ee13f0b22735e26da5e4dd42ae76f924dcf06
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-01-08 08:39:41 -0300

    funcao

--- funcao/funcao.js
+++ funcao/funcao.js
@@ -1,4 +1,23 @@
-function soma2_10(){
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="CBF Curso"
+    d2.innerHTML="CBF Curso"
+    d3.innerHTML="CBF Curso"
+}
+
+
+
+
+
+
+
+
+
+
+
+/*/function soma2_10(){
     let n1=2
     let n2=10
     let soma=n1+n2
@@ -7,4 +26,4 @@ function soma2_10(){
 
 for(let i=0;i<10;i++){
     soma2_10()
-}
+}*/
